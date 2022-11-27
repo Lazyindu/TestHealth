04cc44ebd3806b6f1b9b70c1c71ce992092f4d98
indu <70087467+Lazyindu@users.noreply.github.com>
2022-11-27 10:51:25 +0530

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Lazyindu/TestHealth.git /TestHealth
+  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /TestHealth
+  git clone $UPSTREAM_REPO /Eva
 fi
-cd /TestHealth
+cd /Eva
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
